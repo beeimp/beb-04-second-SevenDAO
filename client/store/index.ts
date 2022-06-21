65bc2084ca114df610c29ab3d811ab3e0027d8c9
BeeImp <zero9231@naver.com>
2022-06-21 23:42:41 +0900

    Fix: redux 타입 추가

--- client/store/index.ts
+++ client/store/index.ts
@@ -5,13 +5,22 @@ const serializableMiddleware = createSerializableStateInvariantMiddleware({
   getEntries: () => [],
 });
 
-const store = () =>
-  configureStore({
-    reducer: {},
-    middleware: [serializableMiddleware],
-    devTools: process.env.NODE_ENV !== 'production',
-  });
+const store = configureStore({
+  reducer: {},
+  middleware: [serializableMiddleware],
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
-export const wrapper = createWrapper(store, {
+const createStore = () => store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+type AppDispatch = typeof store.dispatch;
+
+const wrapper = createWrapper(createStore, {
   debug: process.env.NODE_ENV !== 'production',
 });
+
+export type { RootState, AppDispatch };
+export { wrapper };
