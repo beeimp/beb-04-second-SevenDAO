df8d8c93744e6e91cc9f0f8eb055a6a709080764
ckh7488 <msj74881@gmail.com>
2022-06-22 14:11:57 +0900

    login

---
+++ routes/signup.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import clientPromise from "../lib/mongodb.js";
+import jwtObj from "../lib/jwtObj.js";
+
+const signupRouter = Router();
+
+// 하드코딩파트?
+const dbName = 'nameDB';
+const collectionName = 'name';
+//
+
+signupRouter.get('/', (req, res) => { console.log('get from signup'); res.send('signup') })
+
+
+signupRouter.post('/', (req, res) => {
+    (async () => {
+        const myClient = await clientPromise;
+        const { username, email, password } = req.body;
+        const ret = await myClient.db(dbName).collection(collectionName).find({ username: username }).toArray();   // const ret = await myClient.db().admin().listDatabases()?.then(obj=>obj?.databases);
+        if (ret.length > 0) res.send('username alreay exists');
+        else {
+            // db 저장 파트
+            const jwtStr = jwtObj.jwtSign({username: username});
+            res.cookie('jwt',jwtStr);
+            myClient.db(dbName).collection(collectionName).insertOne({username: username, email: email, password: password});
+            //
+            res.send('signup');
+            
+        }
+    })();
+})
+
+export default signupRouter;
