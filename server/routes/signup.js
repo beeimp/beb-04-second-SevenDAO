215807bbde0bfecba123477bbb4856eadf08f403
ckh7488 <msj74881@gmail.com>
2022-06-23 17:59:19 +0900

    Feat : 게시판 구현중1

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -8,8 +8,8 @@ import wallet from "../lib/wallet.js";
 const signupRouter = Router();
 
 // 하드코딩파트?
-const dbName = 'nameDB';
-const collectionName = 'name';
+const dbName = 'usersDB';
+const collectionName = 'users';
 //
 
 signupRouter.get('/', (req, res) => { console.log('get from signup'); res.send('signup') })
@@ -18,7 +18,7 @@ signupRouter.get('/', (req, res) => { console.log('get from signup'); res.send('
 signupRouter.post('/', (req, res) => {
     (async () => {
         const myClient = await clientPromise;
-        const { username, email, password } = req.body;
+        const { username, email, password } = req.body;6
         const ret = await myClient.db(dbName).collection(collectionName).find({ username: username }).toArray();   // const ret = await myClient.db().admin().listDatabases()?.then(obj=>obj?.databases);
         if (ret.length > 0) res.json({message: "signup fail"});
         else {
