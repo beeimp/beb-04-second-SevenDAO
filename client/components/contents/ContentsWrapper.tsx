f64b61b8e7fa70e7206382048c99520ba7739727
heesu <huisu121@gmail.com>
2022-06-23 14:45:49 +0900

    Feat: contents 컴포넌트 ui 구현

---
+++ client/components/contents/ContentsWrapper.tsx
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+import { FunctionComponent, ReactNode } from 'react';
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const wrapperStyle = css`
+  display: flex;
+  flex-direction: row;
+  min-width: 180px;
+  width: 100%;
+  padding: 10px;
+
+  border-right: 1px solid #a0a0a0ff;
+`;
+
+const ContentsWrapper: FunctionComponent<WrapperProps> = ({ children }) => {
+  return <div css={wrapperStyle}>{children}</div>;
+};
+
+export default ContentsWrapper;
---
+++ client/components/contents/ProfileCard.tsx
@@ -0,0 +1,47 @@
+import { css } from '@emotion/react';
+import { FunctionComponent } from 'react';
+import Img from 'next/image';
+
+interface ProfileCardProps {
+  iconUrl: string;
+  writer: string;
+  size: string;
+}
+
+const ProfileCard: FunctionComponent<ProfileCardProps> = ({ iconUrl, writer, size = '30px' }) => {
+  const wrapperStyle = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 10px;
+  `;
+
+  const userIconStyle = css`
+    background-color: #e5e8eb;
+    width: ${size};
+    height: ${size};
+    border-radius: 50%;
+    margin-right: 10px;
+    cursor: pointer;
+
+    :hover {
+      box-shadow: rgb(0 0 0 / 30%) 0 0 10px;
+      transition: 0.2s ease;
+    }
+  `;
+  const usernameStyle = css`
+    color: #292929ff;
+    font-size: 15px;
+  `;
+
+  return (
+    <div css={wrapperStyle}>
+      <div css={userIconStyle}>
+        <Img src={iconUrl} alt="userIcon" width={size} height={size} />
+      </div>
+      <div css={usernameStyle}>{writer}</div>
+    </div>
+  );
+};
+
+export default ProfileCard;
