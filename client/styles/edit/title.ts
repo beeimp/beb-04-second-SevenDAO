c389d7240a3ecde96a308f8343212495c4347c59
BeeImp <zero9231@naver.com>
2022-06-23 17:01:50 +0900

    Style: 에디터 제목 스타일

---
+++ client/styles/edit/title.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+const editTitleWrapperStyle = css`
+  font-size: 20px;
+`;
+const editTitleLabelStyle = css``;
+const editTitleInputStyle = css`
+  font-size: 2em;
+  border: none;
+  width: 100%;
+  padding: 0.3em;
+  &::placeholder {
+    color: #e2e2e2;
+  }
+`;
+
+export { editTitleWrapperStyle, editTitleLabelStyle, editTitleInputStyle };
