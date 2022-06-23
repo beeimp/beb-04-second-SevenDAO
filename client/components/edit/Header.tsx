a210e0b4761c981b986f590f7adab2a25a9bb122
BeeImp <zero9231@naver.com>
2022-06-23 17:00:09 +0900

    Feat: 에디터 UI 컴포넌트 구현

---
+++ client/components/edit/Header.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface EditHeaderProps {
+
+}
+
+const EditHeader: FunctionComponent<EditHeaderProps> = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default EditHeader;
