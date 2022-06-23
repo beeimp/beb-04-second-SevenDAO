04c4aec0f78015590c413dae35a189a8e05491b4
heesu <huisu121@gmail.com>
2022-06-23 17:07:14 +0900

    Refactor: 타입 추가

--- client/types/post.ts
+++ client/types/post.ts
@@ -1,9 +1,8 @@
-interface PostType {
+export interface PostType {
   post_id: number;
   writer: string;
   title: string;
-  category: 'All' | 'Programming' | 'Media' | 'Technology' | 'Community' | 'Others';
+  contents: string;
+  tag: 'All' | 'Programming' | 'Media' | 'Technology' | 'Community' | 'Others';
   create_date: number;
 }
-
-export type { PostType };
