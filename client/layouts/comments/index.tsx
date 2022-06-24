ac150e47b167e01a8786de6465d31324bf6eec1b
BeeImp <zero9231@naver.com>
2022-06-24 23:30:13 +0900

    Sylte: 스타일 수정

--- client/layouts/comments/index.tsx
+++ client/layouts/comments/index.tsx
@@ -2,13 +2,17 @@ import { FunctionComponent, useEffect } from 'react';
 import { commentsDummyData } from '../../test/dummyData';
 import CommentWrite from './write';
 import Comment from './comment';
+import { css } from '@emotion/react';
 
 interface CommentsProps {}
 
 const Comments: FunctionComponent<CommentsProps> = () => {
   const comments = commentsDummyData;
+  const wrapperStyle = css`
+    margin: 10px;
+  `;
   return (
-    <div>
+    <div css={wrapperStyle}>
       <CommentWrite></CommentWrite>
       {comments.map((comment, index) => (
         <Comment key={index} comment={comment}></Comment>
