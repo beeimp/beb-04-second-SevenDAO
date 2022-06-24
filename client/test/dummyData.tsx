bffcdaf319755013f1b80609a191c4fd4a76a43d
heesu <huisu121@gmail.com>
2022-06-24 22:22:27 +0900

    Test: 더미데이터 imgUrl 추가

--- client/test/dummyData.tsx
+++ client/test/dummyData.tsx
@@ -2,47 +2,52 @@ import { PostType } from '../types/post';
 
 export const dummyData: PostType[] = [
   {
-    _id: '가',
+    _id: '62b52b22ea74e4428f5bc931',
     username: 'FadinGeek',
     title: 'The 6 Most Important Requirements Practices',
     contents:
       'The TL;DR for a 640-page book on software requirements — Authors sometimes make things longer and more complicated than necessary. Some readers might feel that I’ve been guilty of this myself. The third edition',
     tag: 'Others',
     created_date: 1665992800,
+    imgUrl: '/img/1.png',
   },
   {
-    _id: '나',
+    _id: '62b52b22ea74e4428f5bc938',
     username: 'Wiserword',
     title: 'AbacusBasic: don’t wait for an Apple iKeyboard, get this instead',
     contents:
       'Pentaform’s computer-in-a-keyboard available right now, so what can consumers expect of it? — Remember that hypothetical computer-in-a-',
     tag: 'Media',
     created_date: 1655992810,
+    imgUrl: '/img/2.png',
   },
   {
-    _id: '다',
+    _id: '62b52b22ea74e4428fdbc931',
     username: 'Kostas Farkonas',
     title: 'How I Make $5,000 Each Month Writing About Tiny Topics',
     contents:
       'Tiny topics are the tiny houses of profitable blogging content — I make $5,000 or more each month from tiny topics. The tinier, usually the bette...',
     tag: 'All',
     created_date: 1685992800,
+    imgUrl: '/img/3.png',
   },
   {
-    _id: '라',
+    _id: '62b52b22ea74e4428f5bc981',
     username: 'Fotis Adamakis',
     title: 'Vue 3 was a mistake that we should not repeat',
     contents: 'More than 4 years have passed since the initial introduction of Vue 3. …',
     tag: 'Community',
     created_date: 1655994800,
+    imgUrl: '/img/5.png',
   },
   {
-    _id: '마',
+    _id: '62b52b22ea74e5428f5bc931',
     username: 'Melih Gungor',
     title: 'With Pixel 7 Coming Out, Is Pixel 6 Worth It?',
     contents:
       'The Answer Is Quite Simple — During Google I/O 2022, Google just decided to go ahead and unveil the Pixel 7 Series nearly five months before its release. I was really excited to see the new models and I’m sure that a',
     tag: 'All',
     created_date: 1655992800,
+    imgUrl: '/img/8.png',
   },
 ];
