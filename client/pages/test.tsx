aa6789a2bf034089af4b7426bb75d695b4cdf219
BeeImp <zero9231@naver.com>
2022-06-25 00:31:23 +0900

    Test: 출력 테스트

---
+++ client/pages/test.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next/types';
+import Comments from '../layouts/comments';
+
+const TestPage: NextPage = () => {
+  return (
+    <>
+      <Comments></Comments>
+    </>
+  );
+};
+
+export default TestPage;
