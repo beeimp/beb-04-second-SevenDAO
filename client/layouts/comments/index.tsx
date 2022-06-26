14fca3f97cbc8b21bda928104b6338fa7fdf140b
BeeImp <zero9231@naver.com>
2022-06-26 23:21:39 +0900

    Feat: 댓글 없음 추가

--- client/layouts/comments/index.tsx
+++ client/layouts/comments/index.tsx
@@ -3,20 +3,30 @@ import { commentsDummyData } from '../../test/dummyData';
 import CommentWrite from './write';
 import Comment from './comment';
 import { css } from '@emotion/react';
+import { CommentType } from '../../types/comment';
 
-interface CommentsProps {}
+interface CommentsProps {
+  comments: CommentType[];
+}
 
-const Comments: FunctionComponent<CommentsProps> = () => {
-  const comments = commentsDummyData;
+const Comments: FunctionComponent<CommentsProps> = ({ comments }) => {
   const wrapperStyle = css`
     margin: 25px;
   `;
+
+  const emptyStyle = css`
+    text-align: center;
+    color: gray;
+  `;
+
   return (
     <div css={wrapperStyle}>
       <CommentWrite></CommentWrite>
-      {comments.map((comment, index) => (
-        <Comment key={index} comment={comment}></Comment>
-      ))}
+      {comments.length > 0 ? (
+        comments.map((comment, index) => <Comment key={index} comment={comment}></Comment>)
+      ) : (
+        <div css={emptyStyle}>댓글이 없습니다.</div>
+      )}
     </div>
   );
 };
