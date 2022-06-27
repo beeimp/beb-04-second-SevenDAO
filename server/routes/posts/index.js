931ce951baaaac630cbd6a41ad54f4280bd10567
ckh7488 <msj74881@gmail.com>
2022-06-27 13:52:02 +0900

    Feat : 사용자가 작성한 게시글 조회

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -9,7 +9,7 @@ const postsCollectionName = 'posts';
 
 // routers
 import postRouter from './post.js'
-import commentRouter  from './comment.js'
+import commentRouter from './comment.js'
 import commentsRouter from './comments.js'
 
 // url : /post/
@@ -17,29 +17,41 @@ const router = express.Router();
 
 
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     console.log(req.query);
-    const {pageNum, count } = req.query;
+    const { pageNum, count } = req.query;
+    const {username} = req.query;
     // 이름 바꾸는 부분
-    const pageNumber = parseInt(pageNum);
-    const nPerPage = parseInt(count);
+    console.log(pageNum, count, username)
+    if(pageNum !== undefined && count !== undefined) {
+        const pageNumber = parseInt(pageNum);
+        const nPerPage = parseInt(count);
 
-    // db query
-    const myClient = await clientPromise;
-    const dbQueryRes = await myClient.db(postsDBName).collection(postsCollectionName).find()
-        .skip(pageNumber > 0 ? ( ( pageNumber - 1 ) * nPerPage ) : 0)
-        .sort({create_date:1})
-        .limit(nPerPage)
-        .toArray();
+        // db query
+        const myClient = await clientPromise;
+        const dbQueryRes = await myClient.db(postsDBName).collection(postsCollectionName).find()
+            .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
+            .sort({ create_date: 1 })
+            .limit(nPerPage)
+            .toArray();
 
-    res.status(200).send(dbQueryRes.map(obj=> {
-        obj._id = obj._id.toString();
-        return obj
-    }))
-    
-    console.log(dbQueryRes[0]._id?.toString());
+        res.status(200).send(dbQueryRes.map(obj => {
+            obj._id = obj._id.toString();
+            return obj
+        }))
 
-    // res.status(200).send('hi');  
+        console.log(dbQueryRes[0]._id?.toString());
+
+        // res.status(200).send('hi');  
+        return ;
+    }
+    if(username !== undefined) {
+        const myClient = await clientPromise;
+        const dbQueryRes = await myClient.db(postsDBName).collection(postsCollectionName).find({username : username}).toArray();
+
+        res.send(dbQueryRes);
+        return ;
+    }
 
 });
 
@@ -50,7 +62,7 @@ router.use('/comments', commentsRouter);
 // export default {
 //     index: router.get('/', (req,res)=>{console.log(`get from /posts `); res.status(200).send('hi')  })
 //     , posts : postRouter
-    
+
 // }
 
 export default router;
