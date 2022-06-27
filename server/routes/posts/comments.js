975ba24704d9847b380735b0a86fb44ccfe9b376
ckh7488 <msj74881@gmail.com>
2022-06-27 13:42:20 +0900

    Feat : comments 유저네임 검색

--- server/routes/posts/comments.js
+++ server/routes/posts/comments.js
@@ -15,26 +15,40 @@ const commentsCollectionName = 'comments';
 const commentsRouter = Router();
 
 commentsRouter.get('/', async (req, res) => {
-    console.log(req.query);
-    /*
-      { postId: '123' }
-    */
+  console.log(req.query);
+  /*
+    { postId: '123' }
+  */
+  const queryKeys = Object.keys(req.query);
+  if (queryKeys.length > 1) { res.send({ message: 'wrong access' }); return; }
+  if (req.query?.postId !== undefined) {
     try {
-        // const jwt = jwtObj.jwtVerify(req.cookies?.jwt);
-        // console.log(jwt);
+      // const jwt = jwtObj.jwtVerify(req.cookies?.jwt);
+      // console.log(jwt);
 
-        const myClient = await clientPromise;
-        const { postId } = req.query;
-        console.log(postId);
-        const findRes = await myClient.db(commentsDBName).collection(commentsCollectionName).find({ postId : postId }).toArray();
-        // console.log(findRes);
+      const myClient = await clientPromise;
+      const { postId } = req.query;
+      console.log(postId);
+      const findRes = await myClient.db(commentsDBName).collection(commentsCollectionName).find({ postId: postId }).toArray();
+      // console.log(findRes);
 
-        // if (insertRes.acknowledged === true) res.send({ message: "ok" });
-        res.send(findRes);
+      // if (insertRes.acknowledged === true) res.send({ message: "ok" });
+      res.send(findRes);
 
     }
     catch (err) { res.send({ message: "error" }); }
-
+  }
+  if (req.query?.username !== undefined) {
+    try {
+      // const jwt = jwtObj.jwtVerify(req.cookies?.jwt);
+      // console.log(jwt);
+      const username = req.query.username;
+      // if(jwt.username !== username) { res.send({message : 'wrong access'}); return; }
+      const myClient = await clientPromise;
+      const findRes = await myClient.db(commentsDBName).collection(commentsCollectionName).find({ username: username }).toArray();
+      res.send(findRes);
+    } catch (err) { res.send({ message: 'error!' }) }
+  }
 })
 
 
