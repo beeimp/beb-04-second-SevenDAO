2e26c632ec1206a441694e75d8f6fe5d4ad0143c
heesu <huisu121@gmail.com>
2022-06-28 16:47:53 +0900

    Feat: 검색 페이지 구현

---
+++ client/components/search/PostSearch.tsx
@@ -0,0 +1,95 @@
+import { css } from '@emotion/react';
+import { FunctionComponent, useState, useEffect, MouseEvent } from 'react';
+import ContentsWrapper from '../../components/contents/ContentsWrapper';
+import ProfileCard from '../../components/contents/ProfileCard';
+import Title from '../../components/contents/Title';
+import ContentsText from '../../components/contents/ContentsText';
+import Date from '../../components/contents/Date';
+import ImgBox from '../../components/contents/ImgBox';
+import CategoryButton from '../../components/contents/CategoryButton';
+import RecommendationSign from '../../components/contents/RecommendationSign';
+import { SearchResultType } from '../../types/post';
+import { useRouter } from 'next/router';
+
+interface LayoutProps {
+  searchs: SearchResultType[];
+}
+
+const PostSearch: FunctionComponent<LayoutProps> = ({ searchs }) => {
+  const router = useRouter();
+
+  const shortenContents = (contents: string) => {
+    if (contents.length > 60) {
+      return `${contents.slice(0, 60)}...`;
+    } else {
+      return contents;
+    }
+  };
+
+  const textWrapperStyle = css`
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    align-content: center;
+    max-width: 800px;
+  `;
+  const wrapperRowStyle = css`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 10px;
+  `;
+  const wrapperColStyle = css`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+    cursor: pointer;
+  `;
+  const imgWrapperStyle = css`
+    display: flex;
+    align-content: center;
+  `;
+
+  return (
+    <>
+      {searchs
+        .sort((a, b) => b.created_date - a.created_date)
+        .map((content) => {
+          return (
+            <ContentsWrapper key={content._id}>
+              <div
+                css={textWrapperStyle}
+                onClick={() => {
+                  // 클릭 시 postId에 해당하는 상세페이지로 라우팅
+                  router.push(`/detail/${content._id}`);
+                }}
+              >
+                <div css={wrapperRowStyle}>
+                  <ProfileCard
+                    iconUrl={'/sevendao-logo.png'}
+                    writer={content.username}
+                    size={'44px'}
+                  />
+                  <Date created_date={content.created_date} />
+                </div>
+                <div>
+                  <div css={wrapperColStyle}>
+                    <Title title={content.title} />
+                    <ContentsText contents={shortenContents(content.contents)} />
+                  </div>
+                  <div css={wrapperRowStyle}>
+                    <CategoryButton category={content.tag} />
+                    <RecommendationSign />
+                  </div>
+                </div>
+              </div>
+              <div css={imgWrapperStyle}>
+                <ImgBox imgUrl={content.imgUrl ?? ''} size={'200px'} />
+              </div>
+            </ContentsWrapper>
+          );
+        })}
+    </>
+  );
+};
+
+export default PostSearch;
---
+++ client/store/searchSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PostType, SearchType } from '../types/post';
+
+// Define the initial state using that type
+const initialState: SearchType = {
+  searchWord: '',
+  recentSearchArr: [],
+};
+
+export const searchSlice = createSlice({
+  name: 'search',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setSearchword: (state, action: PayloadAction<string>) => {
+      state.searchWord = action.payload;
+    },
+    addRecentSearchArr: (state, action: PayloadAction<string>) => {
+      state.recentSearchArr = [action.payload, ...state.recentSearchArr];
+    },
+  },
+});
+
+export const searchActions = { ...searchSlice.actions };
