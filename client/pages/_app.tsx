10992e5f0b621e0beae95ebaeb40af17e1d5dfcd
BeeImp <zero9231@naver.com>
2022-06-28 14:11:10 +0900

    Feat: JWT 유효성 검사 추가

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,17 +4,24 @@ import { wrapper } from '../store/index';
 import { useEffect } from 'react';
 import Cookie from 'cookie';
 import { authActions } from '../store/authSlice';
-import { parseJwt } from '../utils/jwt';
+import { parseJwt, verifyJwt } from '../utils/jwt';
 import { useDispatch } from 'react-redux';
+import { removeCookies } from 'cookies-next';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const dispatch = useDispatch();
   useEffect(() => {
     const cookies = Cookie.parse(document.cookie ?? '');
 
     if (cookies?.jwt) {
       const token = cookies.jwt;
-      const { username } = parseJwt(token);
+      const { username, iat } = parseJwt(token);
+      if (verifyJwt(iat)) {
+        removeCookies('jwt');
+        router.push('/');
+      }
       dispatch(authActions.successAuth(username));
     } else {
       dispatch(authActions.failureAuth('로그인이 필요합니다.'));
