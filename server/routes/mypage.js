0be322a5c4eb8460d18655d3d02cd18eeddd006a
ckh7488 <msj74881@gmail.com>
2022-06-28 09:52:29 +0900

    Feat : mypage 구현

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -1,3 +1,23 @@
-export default (req,res)=>{
-    res.send('hi im mypage');
+import jwtObj from "../lib/jwtObj.js"
+import clientPromise from "../lib/mongodb.js";
+
+// 하드코딩파트?
+const dbName = 'usersDB';
+const collectionName = 'users';
+//
+
+
+
+export default async (req, res) => {
+    const { jwt } = req.cookies;
+    try {
+        const myJwt = jwtObj.jwtVerify(jwt);
+        const myClient = await clientPromise;
+        const queryRes = await myClient.db(dbName).collection(collectionName).find({username:myJwt.username}).toArray();
+        const {_id,password,...resObj} = queryRes[0];
+        // console.log(queryRes, myJwt, resObj);
+        res.send(resObj);
+
+
+    } catch (e) { res.send({ message: e }) }
 }
