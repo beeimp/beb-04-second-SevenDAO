349adbd2be9a3ec6b2ee52e44202a0eb49e23428
heesu <huisu121@gmail.com>
2022-06-29 11:25:02 +0900

    Feat: 최근검색어 삭제 기능 추가

--- client/layouts/search/SearchEngine.tsx
+++ client/layouts/search/SearchEngine.tsx
@@ -16,11 +16,11 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
   const [list, setList] = useState<SearchResultType[]>([]);
   const [inputValue, setInputValue] = useState<string>('');
   const [recentSearch, setRecentSearch] = useState<string>('');
-  const [recentSearchArr, setRecentSearchArr] = useState<string | null>('');
+  const [recentSearchStr, setRecentSearchStr] = useState<string | null>('');
+  const [remove, setRemove] = useState<boolean>(false);
 
   const getSearchDate = async (value: string) => {
     setRecentSearch(value);
-    //엔터를 누르고 입력값이 있으면 : 최종검색어배열저장 + 서버에 게시물 검색 GET 요청
     const config: AxiosRequestConfig = {
       method: 'get',
       url: `http://localhost:8080/search?searchword=${value}`,
@@ -35,10 +35,10 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
       setList([]);
       setError(true);
     }
-    if (!recentSearchArr) {
-      localStorage.setItem('recentSearchArr', value);
-    } else if (recentSearchArr && !recentSearchArr.split(';;;').includes(value)) {
-      localStorage.setItem('recentSearchArr', recentSearchArr + ';;;' + value);
+    if (!recentSearchStr) {
+      localStorage.setItem('recentSearchStr', value);
+    } else if (recentSearchStr && !recentSearchStr.split(';;;').includes(value)) {
+      localStorage.setItem('recentSearchStr', recentSearchStr + ';;;' + value);
     }
   };
 
@@ -51,12 +51,11 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
     event: React.KeyboardEvent<HTMLTextAreaElement>
   ) => {
     try {
+      //엔터를 누르고 입력값이 있으면 : 최종검색어배열저장 + 서버에 게시물 검색 GET 요청
       if (event.code === 'NumpadEnter' || event.code === 'Enter') {
         if (inputValue === '') {
           return alert('검색어를 입력해주세요.');
         }
-        // console.log(searchData + '는 없음');
-        // console.log(list[0]._id);
         getSearchDate(inputValue);
       }
     } catch (err) {
@@ -65,8 +64,22 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
   };
 
   useEffect(() => {
-    setRecentSearchArr(localStorage.getItem('recentSearchArr'));
+    setRecentSearchStr(localStorage.getItem('recentSearchStr'));
   }, [inputValue]);
+
+  useEffect(() => {
+    setRecentSearchStr(localStorage.getItem('recentSearchStr'));
+  }, [remove]);
+
+  const removeRecentSearch = (el: string) => {
+    const newStr: string =
+      recentSearchStr
+        ?.split(';;;')
+        .filter((i) => i !== el)
+        .join(';;;') ?? '';
+    localStorage.setItem('recentSearchStr', newStr);
+    setRemove(!remove);
+  };
 
   const coverStyle = css`
     display: flex;
@@ -88,11 +101,16 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
     justify-content: center;
     border-radius: 25px;
     font-size: 25px;
-    padding: 10px;
+    padding: 7px 20px;
     cursor: pointer;
     background-color: #1a8817ff;
     color: white;
     border: none;
+  `;
+
+  const closeButtonStyle = css`
+    color: #191919ff;
+    padding-left: 20px;
   `;
 
   return (
@@ -105,15 +123,20 @@ const SearchEngine: FunctionComponent<SearchEngineProps> = () => {
             setInputValue(e.target.value);
           }}
         ></Input>
-        {inputValue === '' ? (
+        {inputValue === '' && recentSearchStr !== '' ? (
           <div>
             <PreTitle />
             <div css={buttonWrapperStyle}>
-              {recentSearchArr?.split(';;;').map((el, index) => (
-                <button key={index} onClick={() => clickRecentSearch(el)} css={buttonStyle}>
-                  {el}
-                </button>
-              ))}
+              {recentSearchStr !== ''
+                ? recentSearchStr?.split(';;;').map((el, index) => (
+                    <button key={index} css={buttonStyle}>
+                      <span onClick={() => clickRecentSearch(el)}>{el}</span>
+                      <span css={closeButtonStyle} onClick={() => removeRecentSearch(el)}>
+                        &times;
+                      </span>
+                    </button>
+                  ))
+                : null}
             </div>
           </div>
         ) : null}
