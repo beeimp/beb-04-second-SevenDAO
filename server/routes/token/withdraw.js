2a54fd984f43cbb8813852d65e8e26d2a1bfce14
ckh7488 <msj74881@gmail.com>
2022-06-30 01:35:44 +0900

    Fix : exchange, withdraw 롤백 및 기본기능 구현 완료 (테스트 필요)

--- server/routes/token/withdraw.js
+++ server/routes/token/withdraw.js
@@ -1,6 +1,6 @@
 import jwtObj from "../../lib/jwtObj.js"
 import clientPromise from "../../lib/mongodb.js";
-import { sendToken, getTOKENBalanceOf } from "../../lib/tokenLib.js";
+import { hasGasFee, sendEthGas, sendToken } from "../../lib/tokenLib.js";
 
 
 // 하드코딩파트?
@@ -8,8 +8,6 @@ const dbName = 'usersDB';
 const collectionName = 'users';
 // 토큰 교환시 수수료
 const trxFee = 10;
-//컨트랙트 오너 pubkey
-const contractOwnerPubKey = '0x384b2E84f40551F19c6C2Ce97b6C1991e3991088';
 //
 
 export default async (req, res) => {
@@ -27,75 +25,25 @@ export default async (req, res) => {
         // const toUser = await myClient.db(dbName).collection(collectionName).find({ username: toUsername }).toArray();
         // console.log(typeof fromUser[0].token, typeof value);
         console.log(fromUser[0]);
-        if (fromUser.length > 1) { res.send({ message: 'db found multiple users' }); return; }
-
-        // cToken 확인 및 전체 trx 가격 계산
-        const fromUserCtoken = await getTOKENBalanceOf(fromUser[0].address);
-        const totalFee = value + trxFee;
-        //
-
-        if ((fromUser[0].token + fromUserCtoken) < totalFee) {
+        if (fromUser[0].token < (value +trxFee)) {
             res.send({ message: 'you do not have enough tokens ' });
             return;
         }
-        // 받는사람 사이트 유저인지 확인
-        const isOurUser = await myClient.db(dbName).collection(collectionName).find({ address: toAddress }).toArray();
-        if (isOurUser.length > 0) { res.send({ message: `this address is our site's user. please use exchange api` }); return; }
+        const isOurUser = await myClient.db(dbName).collection(collectionName).find({address: toAddress}).toArray();
+        if(isOurUser.length > 0) {res.send({message : 'this address is our user. please use exchange api'}); return;}
         // console.log(toUser[0].address, fromUser[0].privatekey);
-        //
 
-        // fromUserCtoken 이 있는 경우
-        if (fromUserCtoken) {
-            const residue = totalFee - fromUserCtoken > 0 ? totalFee - fromUserCtoken : 0;
-            if (residue > 0) {
-                console.log('lastTrxRes');
-                let dbres;
-                const isSendTokenOk = await sendToken(fromUserCtoken, contractOwnerPubKey, fromUser[0].privatekey);
-                if(isSendTokenOk) { dbres = await myClient.db(dbName).collection(collectionName).updateOne({ username: fromUsername }, { $inc: { token: -residue }, }, { upsert: true }); }
-                if (dbres) {
-                    res.send({ message: 'transaction success!' });
-                    return;
-                }
-                else { res.send({message : "transaction failed on db"}) }
-            }
-            else {
-                const lastTrxRes = new Promise.all([
-                    sendToken(totalFee, contractOwnerPubKey, fromUser[0].privatekey),
-                    sendToken(value, toAddress)
-                ])
-
-                if (lastTrxRes[0] === true && lastTrxRes[1] === true) {
-                    res.send({ message: 'transaction success!' });
-                    return;
-                }
-            }
+        // if( ! await hasGasFee(fromUser[0].address) ) await sendEthGas(fromUser[0].address);
+        const trxRes = await sendToken(value, toAddress);
+        
+        if (trxRes) {
+            await myClient.db(dbName).collection(collectionName).updateOne({ username: fromUsername }, { $inc: { token: -(value+trxFee) },}, {upsert : true});
+            
+            res.send({ message: 'transaction success!' });
+            return;
         }
+        res.send({ message: 'trx error' });
 
-        else {
-            const trxRes = await sendToken(value, toAddress);
-            if (trxRes) {
-                await myClient.db(dbName).collection(collectionName).updateOne({ username: fromUsername }, { $inc: { token: -(value + trxFee) }, }, { upsert: true });
-
-                res.send({ message: 'transaction success!' });
-                return;
-            }
-            res.send({ message: 'trx error' });
-        }
 
     } catch (e) { res.send({ message: e }) }
 }
-
-
-
-
-
-// import { Router } from "express";
-
-// const balanceRouter = Router();
-
-// balanceRouter.get('/',(req,res)=>{
-//     res.send('hi');
-// })
-
-
-// export default balanceRouter;
