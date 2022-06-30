9610b9ef2c48e15666c4f05878761967b0402f8e
ckh7488 <msj74881@gmail.com>
2022-06-30 14:15:24 +0900

    Feat : 게시글 검색 페이징 추가

--- server/routes/search.js
+++ server/routes/search.js
@@ -10,9 +10,13 @@ const postsCollectionName = 'posts';
 
 searchRouter.get('/', async (req,res)=>{
     // console.log(req.query);
-    const { searchword } = req.query;
+    const { searchword, pageNum, count } = req.query;
+    
     try{
     if( searchword !== undefined){
+        const pageNumber = parseInt(pageNum);
+        const nPerPage = parseInt(count);
+
         const myClient = await clientPromise;
         const dbQueryRes = await myClient.db(postsDBName).collection(postsCollectionName)
         .find({$or: [
@@ -21,7 +25,11 @@ searchRouter.get('/', async (req,res)=>{
             , { content : { $regex : searchword } }
             , { tag : { $regex : searchword } }
         ]})
-        .toArray()
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
+        .sort({ created_date: -1 })
+        .limit(nPerPage)
+        .toArray();
+        
         console.log(dbQueryRes);
         res.send(dbQueryRes);
         return ;
