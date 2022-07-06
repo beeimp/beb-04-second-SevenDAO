9c729ef8ccc0b6414aa2b78f271527798429cea5
BeeImp <zero9231@naver.com>
2022-07-06 23:21:01 +0900

    Refactor: nest.js로 환경 구축

--- server/lib/erc20/contractAddr.js
+++
@@ -1,3 +0,0 @@
-const ContractAddr = "0x384b2E84f40551F19c6C2Ce97b6C1991e3991088";
-
-export default ContractAddr; 
--- server/lib/erc721/contractAddr.js
+++
@@ -1,3 +0,0 @@
-const ContractAddr = "0xDf256973B7f56941349CB4A5C89d49Ff7E21F32C";
-
-export default ContractAddr; 
