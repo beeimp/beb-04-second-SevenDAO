32fa80b22d7578abe80b0a16ea5a50a107761db6
BeeImp <zero9231@naver.com>
2022-07-30 23:15:22 +0900

    Feat: 배포를 위한 도커, 젠킨스 파일 추가

--- server/app.js
+++ server/app.js
@@ -9,9 +9,9 @@ const port = 8080;
 
 app.use(logger('dev'));
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: true,
   credentials: true,
-  methods: ["GET", "POST", "OPTIONS"],
+  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
 }))
 app.use(express.json({limit : "50mb"}));
 app.use(express.urlencoded({limit:"50mb",extended: false}));
